084f9e27f6b7a1a65b3b86b76ff6e8808ccc165b
Rune Langseid <rl@aplia.no>
2014-11-05 20:21:00 +0100

    Working attiny85 sketch. Transmits device id 202 every 5 sec.

--- sketch/energy_meter/energy_meter.ino
+++ sketch/energy_meter/energy_meter.ino
@@ -1,12 +1,23 @@
 
 /** Arduino energy meter
+ * For ATTINY8
+ *
+ * ATMEL ATTINY85 / ARDUINO
+ *                  +-\/-+
+ *            PB5  1|    |8  VCC
+ *   A3 (D 3) PB3  2|    |7  PB2 (D 2) A 1
+ *   A2 (D 4) PB4  3|    |6  PB1 (D 1) PWM
+ *            GND  4|    |5  PB0 (D 0) PWM
+ *
  *
  * Connect
- * Potoresistor D2 - Interrupt 1 on UNO
- * 433,92mhz sender - D8 (DDRB 0)
+ * @todo Potoresistor D2 - Interrupt 1 on UNO ???
+ * 433,92mhz sender - AVR Pin 5 (PB0 - DDRB 0)
+ * Power to sender - AVR Pin 2,3 (PB3, PB4)
  *
  * Requires
  * - TellstickSensor library (http://forum.arduino.cc/index.php?topic=211371.0)
+ * - Board setup for the ARduino IDE - https://github.com/damellis/attiny
  *
  * Notes
  *
@@ -17,22 +28,29 @@
 #include <EEPROM.h>
 #include <TellstickSensor.h>
 
+// Sensor at PB0 (Pin 5)
 TellstickSensor Sensor1(0);
 
-// IR
-int IRledPin =  13;    // LED connected to digital pin 13
-
-volatile int light; // interrupt flag
+// Power pins PB3 & PB4
+int power1 = 3;
+int power2 = 4;
 
 int count; // kwh count
 int subCount; // kwh sub count
 
-//TellstickSensor Sensor1(0);
+volatile int light; // interrupt flag
 
 void setup()
 {
-  Serial.begin(9600);
-  Serial.println("START");
+  // Setup power pins
+  pinMode(power1, OUTPUT);
+  pinMode(power2, OUTPUT);
+
+  // Set sensor device id
+  Sensor1.SetDeviceID(202);
+
+  //Serial.begin(9600);
+  //Serial.println("START");
 
   count = EEPROM.read(0);
   subCount = EEPROM.read(1);
@@ -40,14 +58,25 @@ void setup()
 
   // Setup interrupt routine
   //attachInterrupt(0, handleInterrupt, CHANGE);
-  attachInterrupt(0, handleInterrupt, FALLING);
+//  attachInterrupt(0, handleInterrupt, FALLING);
 
-  Sensor1.SetDeviceID(202);
 
 }
 
 void loop()
 {
+
+  // tiny test - Transmit
+  powerSender(HIGH);
+  delay(10);
+  Sensor1.SetHumidity((uint8_t)55);
+  Sensor1.SetTemperature((int16_t)(10*10));
+  Sensor1.Transmitt();
+  delay(10);
+  powerSender(LOW);
+  delay(5000);
+
+
   // Interrupt triggered
   if (light == 1)
   {
@@ -65,11 +94,11 @@ void loop()
     Sensor1.SetTemperature((int16_t)(subCount*10));
     Sensor1.Transmitt();
 
-    Serial.print("Light - ");
-    Serial.print("Count ");
-    Serial.print(count);
-    Serial.print(" - Subcount ");
-    Serial.println(subCount);
+    //Serial.print("Light - ");
+    //Serial.print("Count ");
+    //Serial.print(count);
+    //Serial.print(" - Subcount ");
+    //Serial.println(subCount);
     light = 0;
     delay(500);
     sei();
@@ -82,3 +111,10 @@ void handleInterrupt()
   cli();
 }
 
+/* Enables the output pins of the AVR that power the RF sender
+ */
+void powerSender(int flag)
+{
+  digitalWrite(power1, flag);
+  digitalWrite(power2, flag);
+}
